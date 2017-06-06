e5d8bc76a61888c5628872615ca30b2e45f05627
ditoslav <dominik.dito@gmail.com>
2017-06-06 20:09:53 +0100

    Angular4 remade with basic guards and routing

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,41 +1,54 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule }    from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/index';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { AlertComponent } from './_directives/index';
+import { AuthGuard } from './_guards/index';
+import { AlertService, AuthenticationService, UserService } from './_services/index';
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModuleModule} from './material-module/material-module.module';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ContentComponent } from './content/content.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-
-import { Injectable, ModuleWithProviders } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const rootRoutes = [
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
-];
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    ContentComponent,
-    HomeComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    BrowserAnimationsModule,
-    MaterialModuleModule,
-    RouterModule.forRoot(rootRoutes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MaterialModuleModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+
+        // providers used to create fake backend
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
